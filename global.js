63aa2ec36721bc413e3de9c1a8123b749ed0dcb9
valencha <nicolmartinez1992@hotmail.com>
2023-04-16 20:47:58 -0300

    Primera entrega JS

--- global.js
+++ global.js
@@ -4,18 +4,18 @@ let precio;
 let resultado = 0;
 let otroPrecio;
 
-do{
-precio = Number(prompt("Ingrese el valor de su producto aquí:"));
-if(Number(precio)){
-function multiplicar(precio, segundoNumero) {
-resultado = precio * segundoNumero
-alert("El valor del producto con el 10% de descuento es:" + " " + resultado)
-}
-multiplicar(precio, 0.90)
-} else {
-alert("El valor ingresado no es correcto")
-}
-otroPrecio = prompt("¿Desea calcular otro valor? S/N") . toUpperCase();
+do {
+    precio = Number(prompt("Ingrese el valor de su producto aquí:"));
+    if (Number(precio)) {
+        function multiplicar(precio, descuento) {
+            resultado = precio * descuento
+            alert("El valor del producto con el 10% de descuento es:" + " " + resultado)
+        }
+    } else {
+        alert("El valor ingresado no es correcto")
+    }
+    multiplicar(precio, 0.90)
+    otroPrecio = prompt("¿Desea calcular otro valor? S/N").toUpperCase();
 } while (otroPrecio == "S")
 
 
